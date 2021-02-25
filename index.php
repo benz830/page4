6402ddc640acb9954bcef90e624ea6a5f23c015a
benz830 <77878804+benz830@users.noreply.github.com>
2021-02-25 19:51:18 +0800

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title> Story of Poverty Page 3 </title>
+</head>
+<body>
+<body background="bbb.jpg" alink="yellow" vlink="yellow">
+<br>
+<b><h1><font color="black"><center><mark> THE STORY OF POVERTY </b></h1></font></center></mark>
+<b><h2><font color="orange">What are the different kinds of poverty that you may hear about? </B></h2></font>
+<br>
+<b><font color="#eb3434">Fuel Poverty </b>
+<br>- People don’t have enough money to adequately heat their homes without falling into poverty. Nearly 221k children in Scotland experience fuel poverty.
+<br>
+<br><b>In-work poverty
+<br>- Where net household earnings are below the income threshold for that household. 470k people in Scotland don't earn the real Living Wage. 182k
+<br>children in Scotland live in poverty despite having one person in their household in work
+<br>
+<br>Food poverty/Food insecurity
+<br>- Poverty is the key issue in the rising numbers of people using foodbanks. Food is very often one of the first items which people cut back on when they
+<br>begin to fall into poverty. 
+<br>
+<br>Housing Poverty
+<br>- The average weekly social rent in Scotland has risen from £42.64 in 2004-05 to £74.44 in 2016-17, in increase of 75 per cent.</b></font></br></b>
+<br>
+<br><b><h2><font color="purple"> Fun Facts </b></h2><font>
+<br>
+<font color="yellow"><b>689 million people live in extreme poverty, surviving on less than $1.90 a day. Children and youth account for two-thirds of the world’s poor, 
+<br>and women represent a majority in most regions. Extreme poverty is increasingly concentrated in sub-Saharan Africa. About 40% of the region’s people live on less than 
+<br>$1.90 a day. Extreme poverty rates nearly doubled in the Middle East and North Africa between 2015 and 2018, from 3.8% to 7.2%, mostly because of crises in Syria and 
+<br>Yemen. Although countries impacted by fragility, crises, and violence are home to about 10% of the world’s population, they account for more than 40% of people living
+<br>in extreme poverty. By 2030, an estimated 67% of the world’s poor will live in fragile contexts. About 70% of people older than 15 wholive in extreme poverty have no 
+<br>schooling or only some basic education.1.3 billion people in 107 developing countries, which account for 22% of the world’s population, live in multidimensional poverty. 
+<br>644 million children are experiencing multidimensional poverty.
+<br><br><p align="center"><img width ="150px" img src="pt.jpg">
+<br><font color="white"><a href="https://scaffoldpoverty.herokuapp.com/?fbclid=IwAR1OndBXgBJQDQEqDP16G-xBHaifgEej223x_0jisnk3wF5ROqN3fSAGoX0"><center> HOME PAGE </a><p></font>
+</body>
+</html>
